044a9145cd7aba520ebdd1b727cb232b9f1de8a1
fangcuidong <fang_cuidong@outlook.com>
2021-12-16 23:10:16 +0800

    some test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      // targets: ['defaults', 'not IE 11'],
+      // additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      // polyfills: [
+      //   'es.promise',
+      //   'es.promise.all-settled',
+      //   'es.promise.any',
+      //   'es.promise.finally',
+      // ],
+      // modernPolyfills: [
+      //   'es.promise',
+      //   'es.promise.all-settled',
+      //   'es.promise.any',
+      //   'es.promise.finally',
+      // ],
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -15,6 +35,12 @@ export default defineConfig({
       scss: {
         additionalData: '@import "@/style/main.scss";',
       },
+    },
+  },
+  server: {
+    // open: true,
+    fs: {
+      // allow: ['path'],
     },
   },
 });
