061795cde46cd8f57ff459f84401a868bcdaff3c
fangcuidong <fang_cuidong@outlook.com>
2021-12-17 23:01:08 +0800

    add doc

--- vite.config.ts
+++ vite.config.ts
@@ -7,29 +7,55 @@ import legacy from '@vitejs/plugin-legacy';
 export default defineConfig({
   plugins: [
     vue(),
+    /**
+     * 为打包后的文件提供传统浏览器兼容性支持
+     *
+     * @see https://cn.vitejs.dev/plugins/#vitejsplugin-legacy
+     */
     legacy({
-      // targets: ['defaults', 'not IE 11'],
-      // additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-      // polyfills: [
-      //   'es.promise',
-      //   'es.promise.all-settled',
-      //   'es.promise.any',
-      //   'es.promise.finally',
-      // ],
-      // modernPolyfills: [
-      //   'es.promise',
-      //   'es.promise.all-settled',
-      //   'es.promise.any',
-      //   'es.promise.finally',
-      // ],
+      targets: ['defaults', 'not IE 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      polyfills: [
+        'es.promise',
+        'es.promise.all-settled',
+        'es.promise.any',
+        'es.promise.finally',
+      ],
+      modernPolyfills: [
+        'es.promise',
+        'es.promise.all-settled',
+        'es.promise.any',
+        'es.promise.finally',
+      ],
     }),
   ],
 
+  /**
+   * 文件系统路径的别名
+   *
+   * @see https://cn.vitejs.dev/config/#resolve-alias
+   */
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  /**
+   * 强制预构建链接的包
+   *
+   * @see https://cn.vitejs.dev/config/#optimizedeps-include
+   */
+  optimizeDeps: {
+    // entries: [],
+    // include
+  },
+
+  /**
+   *  指定传递给 CSS 预处理器的选项
+   *
+   * @see https://cn.vitejs.dev/config/#css-preprocessoroptions
+   */
   css: {
     preprocessorOptions: {
       scss: {
@@ -37,8 +63,21 @@ export default defineConfig({
       },
     },
   },
+
+  /**
+   * 开发服务器选项
+   *
+   * @see https://cn.vitejs.dev/config/#server-options
+   */
   server: {
-    // open: true,
+    // 开发服务器端口
+    port: 3000,
+    // 开发服务器启动时自动在浏览器打开
+    open: 'firefox',
+    // 为开发服务器配置自定义代理规则
+    proxy: {},
+    // 跨域
+    cors: true,
     fs: {
       // allow: ['path'],
     },
